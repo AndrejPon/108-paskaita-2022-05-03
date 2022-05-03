0b98def2609566c1c961f6c2e289fcc6d20236ff
Andrejus Ponomariovas <andrejus.pon@gmail.com>
2022-05-03 18:03:57 +0300

    Car header

---
+++ src/components/CarHeader/CarHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+const CarHeader = (props) => {
+  return <div>CarHeader</div>;
+};
+
+CarHeader.propTypes = {};
+
+export default CarHeader;
---
+++ src/components/Tile/Tile.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tile from './Tile';
+
+export default {
+  title: 'Tile',
+  component: Tile,
+};
+
+export const Primary = () => (
+  <Tile>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi illo nemo ea
+    laborum quas! Vel dicta ipsam non incidunt commodi, omnis porro saepe
+    praesentium reiciendis, officiis minima veritatis recusandae deserunt.
+  </Tile>
+);
